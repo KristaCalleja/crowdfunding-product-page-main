de5c0d2fd019f3e8b8abdd594a2aff3f30398c94
KristaCalleja <kristacm@gmail.com>
2021-07-02 12:25:24 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,30 @@
+const burger = document.querySelector('.menu-modal')
+const closeMenu = document.querySelector('.close');
+const navMenu = document.querySelector('.nav-menu');
+
+const bookMarkBtn = document.querySelector('.bookmark-btn');
+const bookIcon = document.querySelector('.bookmark-icon');
+
+const callToAction = document.querySelector('button');
+const successModal = document.getElementById('success-modal');
+
+// Toggle mobile menu
+function navShow(){
+    navMenu.classList.toggle('nav-active');
+    burger.classList.toggle('menu-modal-active');
+}
+burger.addEventListener('click', navShow);
+
+// Toggle bookmark
+function toggleBookMark(){
+    bookMarkBtn.classList.toggle('bookmark-btn-active');
+    bookIcon.classList.toggle('bookmark-icon-active');
+}
+bookMarkBtn.addEventListener('click', toggleBookMark);
+bookIcon.addEventListener('click', toggleBookMark);
+
+// Success modal
+function onOpen(){
+    typeof successModal.showModal();
+}
+callToAction.addEventListener('click', console.log('open'));
