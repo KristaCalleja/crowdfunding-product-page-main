13f616747acf502b062427d8e94114b4dfbd81c1
KristaCalleja <kristacm@gmail.com>
2021-08-04 12:29:35 -0300

    Success modal

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const bookMarkBtn = document.querySelector('.bookmark-btn');
 const bookIcon = document.querySelector('.bookmark-icon');
 
 const callToAction = document.querySelector('button');
-const successModal = document.getElementById('success-modal');
+const successModal = document.getElementById('modal-completed');
+const modalOuter = document.querySelector('.modal-outer');
 const wrapper = document.querySelector('.wrapper');
 
 // Toggle mobile menu
@@ -19,13 +20,33 @@ burger.addEventListener('click', navShow);
 // Toggle bookmark
 function toggleBookMark(){
     bookMarkBtn.classList.toggle('bookmark-btn-active');
+    bookMarkBtn.textContent = 'Bookmarked';
     bookIcon.classList.toggle('bookmark-icon-active');
 }
 bookMarkBtn.addEventListener('click', toggleBookMark);
 bookIcon.addEventListener('click', toggleBookMark);
 
 // Success modal
-function onOpen(){
-    alert(successModal.textContent);
+function openSuccessModal(){
+    modalOuter.classList.add('open');
 }
-callToAction.addEventListener('click', onOpen);
+callToAction.addEventListener('click', openSuccessModal);
+
+// Close success modal
+function closeSuccessModal(){
+    modalOuter.classList.remove('open');
+}
+modalOuter.addEventListener('click', function(event){
+    console.log(event.target);
+    console.log(event.currentTarget);
+    const isOutside = !event.target.closest('modal-completed');
+    if (isOutside){
+        closeSuccessModal();
+    }
+});
+
+window.addEventListener('keydown', (event) => {
+    if(event.key == 'Escape'){
+        closeSuccessModal();
+    }
+})
